e7a674c893bb2573a0e34b8eb518a9d07374caec
Thuc Tran <thuc.ttk@gmail.com>
2024-09-20 13:00:45 +0700

    [update] img url with tinify cdn

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -152,10 +152,10 @@ const GalleryInstance = ({ popupDan, cate = 'wedding', photos = [] }) => {
           key={photo}
           className="picture"
           data-sub-html={`<h4>${title[cate]}</h4>`}
-          data-src={`https://fifth-furtive-hoverfly.glitch.me/images/${photo}.png`}
+          data-src={`https://b1560601.tinifycdn.com/${photo}.png`}
         >
           <img
-            src={`https://fifth-furtive-hoverfly.glitch.me/images/${photo}.png`}
+            src={`https://b1560601.tinifycdn.com/${photo}.png`}
           />
         </div>
         )
