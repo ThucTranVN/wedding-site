6b436a8b9ee93e9dfa8ab096c2083622919a04fe
Thuc Tran <thuc.ttk@gmail.com>
2024-09-21 12:09:53 +0700

    [revert] gallery loading images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -152,10 +152,10 @@ const GalleryInstance = ({ popupDan, cate = 'wedding', photos = [] }) => {
           key={photo}
           className="picture"
           data-sub-html={`<h4>${title[cate]}</h4>`}
-          data-src={`https://b1560601.tinifycdn.com/${photo}.png`}
+          data-src={`https://fifth-furtive-hoverfly.glitch.me/images/${photo}.png`}
         >
           <img
-            src={`https://b1560601.tinifycdn.com/${photo}.png`}
+            src={`https://fifth-furtive-hoverfly.glitch.me/images/${photo}.png`}
           />
         </div>
         )
